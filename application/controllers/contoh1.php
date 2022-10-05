3e7756f2aaa65e4a75d86f033ae7dd860325b097
radendivi <113522278+radendivi@users.noreply.github.com>
2022-10-05 20:16:43 +0700

    Add files via upload

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php 
+class Contoh1 extends  CI_Controller
+{
+    public function index()
+    { 
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama Saya Divi Harpiadi
+        saya tinggal di daerah Tangerang
+        olahraga yang saya sukai adalah Sepakbola";
+    }
+}
---
+++ application/views/view_latihan.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>latihan 1</title>
+</head>
+<body>
+    Halo yuk belajar<br>
+    NIlai 1 = <?= $nilai1; ?> ,
+    Nilai 2 = <?= $nilai2; ?> 
+    Ini Hasil, Yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?> 
+   
+   
+</body>
+</html>
